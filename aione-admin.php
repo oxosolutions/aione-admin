b63c181f6d133d3db71bbf0d3d71d155f2b49ca7
SGS Sandhu <sgs.sandhu@gmail.com>
2018-08-07 16:47:03 +0530

    Update

--- aione-admin.php
+++ aione-admin.php
@@ -1,25 +1,14 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://oxosolutions.com/
- * @since             1.0.0
- * @package           Aione_Admin
- *
  * @wordpress-plugin
  * Plugin Name:       Aione Admin
- * Plugin URI:        http://oxosolutions.com/products/wordpress-plugins/aione-admin/
+ * Plugin URI:        https://oxosolutions.com/products/wordpress-plugins/aione-admin/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.6.7
+ * Version:           1.0.6.8
  * Author:            OXO Solutions®
  * Author URI:        https://oxosolutions.com/
- * License:           GPL-2.0+
+ * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       aione-admin
  * Domain Path:       /languages

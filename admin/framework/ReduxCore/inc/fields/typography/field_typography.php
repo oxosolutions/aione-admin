5bdb050bedc0c4b36ed8c8040592649b74290bd4
SGS Sandhu <sgs.sandhu@gmail.com>
2025-03-08 16:20:05 +0530

    Fix for php 8.2

--- admin/framework/ReduxCore/inc/fields/typography/field_typography.php
+++ admin/framework/ReduxCore/inc/fields/typography/field_typography.php
@@ -821,10 +821,13 @@ if ( ! class_exists( 'ReduxFramework_typography' ) ) {
                         $arr[ $key ] = $value;
                     }
 
-                    $lcFonts = array_change_key_case( $this->field['custom_fonts'] );
-                    foreach ( $lcFonts as $group => $fontArr ) {
-                        foreach ( $fontArr as $key => $value ) {
-                            $arr[ strtolower( $key ) ] = $key;
+                    // Ensure the fonts array is NOT empty
+                    if ( ! empty( $this->field['custom_fonts'] ) && is_array( $this->field['custom_fonts']  ) ) {
+                        $lcFonts = array_change_key_case( $this->field['custom_fonts'] );
+                        foreach ( $lcFonts as $group => $fontArr ) {
+                            foreach ( $fontArr as $key => $value ) {
+                                $arr[ strtolower( $key ) ] = $key;
+                            }
                         }
                     }
 
